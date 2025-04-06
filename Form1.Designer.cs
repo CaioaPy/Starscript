53686e4c2b0bface978569019f81071173869c0d
yatomikazuchi <caioevilabner@gmail.com>
2025-04-05 22:14:31 -0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace lifeSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl = new TabControl();
+            tabPage1 = new TabPage();
+            button1 = new Button();
+            XP = new Label();
+            tabControl.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl
+            // 
+            tabControl.Controls.Add(tabPage1);
+            tabControl.Location = new Point(0, 0);
+            tabControl.Name = "tabControl";
+            tabControl.SelectedIndex = 0;
+            tabControl.Size = new Size(921, 438);
+            tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(XP);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(913, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "local";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(8, 218);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // XP
+            // 
+            XP.AutoSize = true;
+            XP.Font = new Font("Segoe UI", 20F);
+            XP.Location = new Point(-4, 0);
+            XP.Name = "XP";
+            XP.Size = new Size(172, 37);
+            XP.TabIndex = 1;
+            XP.Text = "Experience: 0";
+            XP.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(922, 450);
+            Controls.Add(tabControl);
+            Name = "Form1";
+            Text = "Life Simulator";
+            Load += Form1_Load;
+            tabControl.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl;
+        private TabPage tabPage1;
+        private Label XP;
+        private Button button1;
+    }
+}
